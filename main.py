118b1259fa3182ce3abcbdb1cef099d1e8081c5c
github-bsaf <67910532+github-bsaf@users.noreply.github.com>
2024-05-15 18:50:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,4 @@ import requests
 import os
 import sys
 
-jenkins_url = 'https://jenkins-gldtn-tv2-sdk.cmo.conti.de'
-job_name = 'pipeline_windows_CCIF'
-build_parameters = {
-   'JIRA_TICKETS': 'value1',
-    # Add more parameters as needed
-}
-
-build_url = f'{jenkins_url}/job/{job_name}/buildWithParameters'
-response = requests.get("https://github.com", auth=('zzzzzzzzzzzzzzzzzzzzzzzz', 'zzzzzzzzzzzzzzzzzzzzzzzzzzzz'))
-print(response.text)
+import zadazda
